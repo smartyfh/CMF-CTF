27637c0e5c2e9a7f8474c11d29a15aad3b6a38f7
Destiny is ALL <smartye_uestc@hotmail.com>
2021-01-22 15:07:36 +0000

    Create setup.py

---
+++ CTF/setup.py
@@ -0,0 +1,27 @@
+import sys
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import shutil
+import numpy
+
+print('Build extension modules...')
+print('==============================================')
+
+ext_modules = [Extension('core',
+				['src/core/core.pyx', 
+				'src/core/CTF.cpp'],
+				language='c++',
+				include_dirs=[numpy.get_include()],
+				extra_compile_args=["-O2"]
+			  )]
+
+setup(
+	name = 'Extended Cython module',
+	cmdclass = {'build_ext': build_ext},
+	ext_modules = ext_modules
+)
+
+shutil.move('core.cpython-37m-x86_64-linux-gnu.so', 'src/core.so')
+print('==============================================')
+print('Build done.\n')
